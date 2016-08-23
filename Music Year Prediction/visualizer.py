d9b1fb98f2d3dbbfab43a4ec857ffa0f66a30f47
SAYAN DAS <poke19962008@gmail.com>
2016-08-23 06:49:13 +0530

    added visualizer

--- Music Year Prediction/visualizer.py
+++ Music Year Prediction/visualizer.py
@@ -1,29 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open('training.bin', 'r') as f:
-    trn = np.load(f)
-    years = np.unique(trn[:,0])
+features = {
+    'loudness': 1,
+    'brightness': 2,
+    'flatness': 3,
+    'attack': 4,
+    'b1': 5,
+    'b2': 6,
+    'b3': 7,
+    'b4': 8,
+    'b5': 9,
+    'b6': 10,
+    'b7': 11,
+    'b8': 12,
+}
 
-    mean = np.array([])
-    for year in years:
-        try:
-            rng = (trn[:,0]==year).nonzero()[0]
+def meanGrapher(ftr):
+    with open('bin/mean.bin', 'r') as f:
+        mean = np.load(f)
+        plt.plot(mean[:,0], mean[:,features[ftr]], 'o', label="mean"+ftr)
+        plt.xlabel('Year')
+        plt.ylabel('Mean of average '+ftr)
+        plt.title('Mean '+ftr)
 
-            avg = np.zeros(90)
-            for i in rng:
-                avg = np.add(avg, trn[i][1:])
-            avg = avg/len(rng)
-            avg = np.append([year], avg)
-            if not len(mean):
-                mean = np.array([avg])
-            else:
-                mean = np.append(mean, np.array([avg]), axis=0)
+def allFourMeanPlotter():
+    meanGrapher('loudness')
+    meanGrapher('brightness')
+    meanGrapher('flatness')
+    meanGrapher('attack')
 
-            print "[SUCCESS] Calculated mean vectors of ", year
-        except:
-            print "[FAILED] COuld not calculate mean for ", year
+def allBPlotter():
+    ftrs = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8']
 
-    with open('mean.bin', 'wb') as fi:
-        np.save(fi, mean)
-        print "[SUCCESS] Saved as `mean.bin` "
+    for ftr in ftrs:
+        plt.subplot(4, 2, ftrs.index(ftr)+1)
+        meanGrapher(ftr)
+
+
+if __name__ == '__main__':
+    # allFourMeanPlotter()
+    allBPlotter()
+
+    plt.legend()
+    plt.show()
