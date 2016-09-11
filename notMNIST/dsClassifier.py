e8be789833882336305d8c23e187cf7410845f2e
SAYAN DAS <poke19962008@gmail.com>
2016-09-11 21:25:29 +0530

    added merge dataset methid

--- notMNIST/dsClassifier.py
+++ notMNIST/dsClassifier.py
@@ -20,18 +20,42 @@ def loadLetter(rootFolder):
                 print "[SUCCESS] Loaded: ", imageFile
                 print "Mean: ", np.mean(image)
                 print "Standard Deviation: ", np.std(image), "\n\n"
-
-                # plt.imshow(image)
-                # plt.show()
             except:
                 print "[FAIL] Cannot Load: ", imageFile, "\n\n"
-
-        #     break
-        # break
         with open('bin/letters/%s.pkl'%label, 'wb') as f:
             pickle.dump(np.array(npImages), f)
+
+def mergeDatasets():
+    trainingSet, validSet, testSet = [], [], []
+
+    pickles = os.listdir("bin/letters")
+
+    for pkl in pickles:
+        with open("bin/letters/%s"%pkl, 'rb') as f:
+            letterSet = pickle.load(f)
+
+            trainSize = int(letterSet.shape[0]*0.8)
+            testSize = int(letterSet.shape[0]*0.2)
+            validSize = int(trainSize*0.2)
+
+            if not len(trainingSet):
+                trainingSet = letterSet[:trainSize,:,:]
+                testSet = letterSet[trainSize:trainSize+testSize,:,:]
+                validSet = letterSet[:validSize,:,:]
+                continue
+
+            trainingSet =np.append(trainingSet, letterSet[:trainSize,:,:], axis=0)
+            testSet = np.append(testSet, letterSet[trainSize:trainSize+testSize,:,:], axis=0)
+            validSet = np.append(validSet, letterSet[:validSize,:,:], axis=0)
+
+    print "Training Set: ", trainingSet.shape
+    print "Validation Set: ", validSet.shape
+    print "Testing Set: ", testSet.shape
+
+    return trainingSet, validSet, testSet
 
 if __name__ == '__main__':
     rootFolder = 'data'
 
-    loadLetter(rootFolder)
+    # loadLetter(rootFolder)
+    mergeDatasets()
