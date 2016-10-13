d0b04dc3c2b72e086d40f16274692abe6352ec63
SAYAN DAS <poke19962008@gmail.com>
2016-10-13 21:20:25 +0530

    added prediction

--- notMNIST/logisticRegression.py
+++ notMNIST/logisticRegression.py
@@ -1,19 +1,53 @@
+# Logistic Regression model
+# Accuracy: 78.689%
+# Training Time: --sec
+# Prediction Time: --sec
+# Number of records:
+
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-import os, pickle
+import matplotlib.image as mpimg
+import os, pickle, time
+
+def model():
+    with open("bin/notMNIST.pkl", 'r') as f:
+        alphabTest = pickle.load(f)
+
+        X = alphabTest["trainingSet"]
+        X = X[:len(X)].reshape(len(X), 784)
+        Y = alphabTest["trainingLabel"]
+        del alphabTest
+
+        logreg = LogisticRegression()
+        start_time = time.time()
+        classifier = logreg.fit(X, Y)
+        print("Training Time: %s sec" % (time.time() - start_time))
+
+        del X, Y
+        with open('bin/lrClssr.pkl', 'wb') as f:
+            pickle.dump(classifier, f)
+            print "[SUCCESS] Saved as lrClssr.pkl"
 
 
-with open("bin/notMNIST.pkl", 'r') as f:
-    alphabTest = pickle.load(f)
+def predict():
+    with open('bin/lrClssr.pkl', 'r') as f:
+        classifier = pickle.load(f)
 
-    X = alphabTest["trainingSet"]
-    X = X[:len(X)].reshape(len(X), 784)
-    Y = alphabTest["trainingLabel"]
-    del alphabTest
+        with open("bin/notMNIST.pkl", 'r') as f:
+            alphabTest = pickle.load(f)
 
-    logreg = LogisticRegression(C=1e5)
-    classifier = logreg.fit(X, Y)
+            X = alphabTest["testSet"]
+            X = X[:len(X)].reshape(len(X), 28*28) # Image Dimension is 28*28
+            Y = alphabTest["testLabel"]
+            del alphabTest
 
-    del X, Y
-    with open('bin/lrClssr.pkl', 'wb') as f:
-        pickle.dump(f, classifier)
+            start_time = time.time()
+            classifier.predict(X)
+            print("Prediction Time: %s sec" % (time.time() - start_time))
+            pred = classifier.score(X, Y)
+
+            print "Accuracy: ", pred
+
+if __name__ == '__main__':
+    model()
+    predict()
