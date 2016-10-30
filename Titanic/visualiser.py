a3523a8fc189fa6226f378ae92cd8f4c8331fe18
SAYAN DAS <poke19962008@gmail.com>
2016-10-30 20:27:17 +0530

    added differenceer

--- Titanic/visualiser.py
+++ Titanic/visualiser.py
@@ -4,7 +4,7 @@ import numpy as np
 
 df = pd.read_csv('train.csv')
 
-def survivalFreq(X='Age'):
+def survivalFreq(X='Age',diff=False):
     groupS = df.groupby(['Survived']).get_group(1)
     groupS = groupS.groupby([X])[X].aggregate(len)
     groupS = pd.DataFrame({X: groupS.index, 'Frequency': groupS.values})
@@ -14,9 +14,13 @@ def survivalFreq(X='Age'):
     groupD = pd.DataFrame({X: groupD.index, 'Frequency': groupD.values})
 
     group = pd.merge(groupD, groupS, how='inner', on=X, suffixes=('Dead', 'Survived'))
-    group[['FrequencyDead', 'FrequencySurvived']].plot.bar(x=group[X], title='%s And Survival Frequency'%X, stacked=True)
+    if not diff:
+        group[['FrequencyDead', 'FrequencySurvived']].plot.bar(x=group[X], title='%s And Survival Frequency'%X, stacked=True)
+    else:
+        group['diff'] = group.FrequencySurvived - group.FrequencyDead
+        group[['diff']].plot.bar(x=group[X], title='%s And Survival Frequency'%X, stacked=True); plt.axhline(0, color='k')
 
 if __name__ == '__main__':
-    survivalFreq('Sex')
+    survivalFreq('Age',diff=True)
 
     plt.show()
